ab6bd48ad6b0ada456093408ace8bfff4638769d
GANESHAN SUDHAKAR <ganeshanbvn1@gmail.com>
2023-09-07 20:09:24 +0530

    push

--- routes/payments/paymentGateways.js
+++ routes/payments/paymentGateways.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const stripe = require('stripe')('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');
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "mysecretkey";
 
 router.get("/razorpay", (req, res) => { //only for razorpay
     res.status(200).json({ ID: "rzp_test_kM6TGFic2ENg3f", "SECRET": "DqzrRqBYxu2b3KW7UMGRTnmL", "AMOUNT": 200 });
@@ -37,7 +39,7 @@ router.post('/subscription', async (req, res) => {
     // Use an existing Customer ID if this is a returning customer.
     const customer = await stripe.customers.create({ phone });
 
-    const ephemeralKey = await stripe.ephemeralKey.create({ customer: customer.id }, { apiVersion: '2023-09-07' })
+    const ephemeralKey = await stripe.ephemeralKeys.create({ customer: customer.id }, { apiVersion: '2023-08-16' })
 
     const subscription = await stripe.subscriptions.create({
         customer: customer.id,
