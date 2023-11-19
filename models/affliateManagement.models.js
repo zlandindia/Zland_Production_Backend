5c4489c299759a360b68403a419f4afa1c579d9c
GANESHAN SUDHAKAR <ganeshanbvn1@gmail.com>
2023-11-19 10:19:25 +0530

    push

--- models/affliateManagement.models.js
+++ models/affliateManagement.models.js
@@ -7,31 +7,38 @@ const affliateManager = new Schema(
         Name: {
             type: String,
             trim: true,
+            require: true,
         },
         Mobile: {
             type: Number,
             unique: true,
             trim: true,
+            require: true,
         },
         AccountNumber: {
             type: String,
             trim: true,
+            require: true,
         },
         Ifsc: {
             type: String,
             trim: true,
+            require: true,
         },
         BankName: {
             type: String,
             trim: true,
+            require: true,
         },
         AffliateCode: {
             type: String,
             trim: true,
+            require: true,
         },
         token: {
             type: String,
             trim: true,
+            require: true,
         }
     },
     { timestamps: true }
